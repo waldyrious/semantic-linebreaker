56b3b282ec2b1a6f1886ddeb02c70b42303904ef
Waldir Pimenta <waldir@email.com>
2013-03-30 12:40:50 +0000

    move css and javascript to separate files

---
+++ actions.js
@@ -0,0 +1,30 @@
+function capturePaste() { 
+  // http://stackoverflow.com/q/2176861/
+  setTimeout(breakPhrases, 1);
+}
+
+function breakPhrases() {
+  var textArea = document.getElementById("textbox");
+  // http://stackoverflow.com/q/4698560/
+  var scrollTop = textArea.scrollTop;
+  // http://stackoverflow.com/q/3286595/
+  var selStart = textArea.selectionStart;
+  var selEnd = textArea.selectionEnd;
+  var manualMode = document.getElementById("manual-mode");
+  // clean up previous runs
+  textArea.value = textArea.value.replace(/^(.{0,72})◀/gm, "$1");
+  // don't do any parsing if we're editing in manual mode
+  if( !manualMode.checked ) {
+    // break on sentence boundaries (hard limits)
+    textArea.value = textArea.value.replace(/(.{20,}?)([.?!][”"]?|[:;]) /g, "$1$2\n");
+    // break on conjunctions
+    textArea.value = textArea.value.replace(/(.{20,}?) (or|and|but|such as|for example,?|e(\. ?)?g\.?|i(\. ?)?e\.?) /g, "$1\n$2 ");
+    // break on clause boundaries (soft limits)
+    textArea.value = textArea.value.replace(/(.{20,}?)(,[”"]?) /g, "$1$2\n");
+    // mark too long lines, if there are any left
+    textArea.value = textArea.value.replace(/^([^◀\n]{72,}?)(?!◀) /gm, "$1◀ ");
+  }
+  textArea.scrollTop = scrollTop;
+  textArea.selectionStart = selStart;
+  textArea.selectionEnd = selEnd;
+}
